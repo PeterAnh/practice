d389d9af5041099ef6eba8c107c3cbedaffae77b
Peter Anh <peteranh3105@gmail.com>
2019-09-16 11:23:39 +0800

    Implement add(element) to XOR Linked list

--- XOR linkedlist/linkedlist.py
+++ XOR linkedlist/linkedlist.py
@@ -2,21 +2,38 @@ class Node:
     def __init__(self, val, prev, next):
         self.index = val
         self.address = prev ^ next
+
+    def next_node(self, prev):
+        next_node = self.address ^ prev
+        return next_node
+    
+    def prev_node(self, next):
+        prev_node = self.address ^ next
     
 
 class XORLinkedList:
     def __init__(self, root):
-        self.root = Node()
+        #Python does not support pointer, hence using the List structure to simulate pointer
+        self.memory = [Node(None,-1,-1)]
+    
+    #Create root node
+    def root(self):
+        return 0, -1, self.memory[0]
     
     def add(self,element):
-        current = self.root
+        current_index, previous_index, current = self.root()
         while(True):
-            if(current.next_node == None):
+            next_index = current.next_node(previous_index)
+            if(next_index == -1):
                 break
-                
-        new_node = Node(element,current)
+            previous_index = current_index
+            current_index = next_index
+            current = self.memory[next_index]
 
-        pass
+        new_node_index = len(self.memory)
+        current_index = previous_index ^ new_node_index
+        new_node = Node(element,current_index,-1)
+        self.memory.append(new_node)
 
     def get(index):
         pass
